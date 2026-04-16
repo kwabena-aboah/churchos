2e103e4613cad037e57e5ccffa53bd7e0530025b
Mintah Obed Aboah <obed30mintah@yahoo.com>
2026-04-16 09:13:38 +0000

    updated

--- backend/churchos/settings/base.py
+++ backend/churchos/settings/base.py
@@ -104,7 +104,11 @@ WSGI_APPLICATION = "churchos.wsgi.application"
 #     }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.parse(
+        config("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # DATABASES = {
